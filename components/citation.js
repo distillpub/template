712d859dad066052c3a81d823830524e106d4850
Christopher Olah <colah@google.com>
2017-01-10 12:42:59 -0800

    more work on citations

--- components/citation.js
+++ components/citation.js
@@ -15,9 +15,10 @@ export default function(dom, data) {
       var cite_string = inline_cite_short(keys);
       var cite_hover_str = "";
       keys.map((key,n) => {
-        if (n>0) cite_hover_str += "<br>";
-        cite_hover_str += bibliography_cite(data.bibliography[key], true);
+        if (n>0) cite_hover_str += "<br><br>";
+        cite_hover_str += hover_cite(data.bibliography[key]);
       });
+      cite_hover_str = cite_hover_str.replace(/"/g, "&#39;")
       el.innerHTML = `<span id="citation-${n}" class="citation" data-hover="${cite_hover_str}">${cite_string}</span>`;
     }
   });
@@ -62,43 +63,76 @@ export default function(dom, data) {
     return keys.map(cite_string).join(", ");
   }
 
+  function author_string(ent, template, sep, finalSep){
+    var names = ent.author.split(" and ");
+    let name_strings = names.map(name => {
+      var last = name.split(",")[0].trim();
+      var firsts = name.split(",")[1];
+      var initials = "";
+      if (firsts != undefined) {
+        initials = firsts.trim().split(" ").map(s => s.trim()[0]);
+        initials = initials.join(".")+".";
+      }
+      return template.replace("F", firsts)
+                     .replace("L", last)
+                     .replace("I", initials);
+    });
+    if (names.length > 1) {
+      var str = name_strings.slice(0, names.length-1).join(sep);
+      str += (finalSep || sep) + name_strings[names.length-1];
+      return str;
+    } else {
+      return name_strings[0];
+    }
+  }
+
+  function venue_string(ent) {
+    var cite = (ent.journal || ent.booktitle || "")
+    if ("volume" in ent){
+      var issue = ent.issue || ent.number;
+      issue = (issue != undefined)? "("+issue+")" : "";
+      cite += ", Vol " + ent.volume + issue;
+    }
+    if ("pages" in ent){
+      cite += ", pp. " + ent.pages
+    }
+    cite += ". "
+    if ("publisher" in ent){
+      cite += ent.publisher + ".";
+    }
+    return cite;
+  }
+
   function bibliography_cite(ent, fancy){
     if (ent){
-      var names = ent.author.split(" and ");
-      var cite = "";
-      let name_strings = names.map(name => {
-        var last = name.split(",")[0].trim();
-        var firsts = name.split(",")[1];
-        if (firsts != undefined) {
-          var initials = firsts.trim().split(" ").map(s => s.trim()[0]);
-          return last + ", " + initials.join(".")+".";
-        }
-        return last;
-      });
-      if (names.length > 1) {
-        cite += name_strings.slice(0, names.length-1).join(", ");
-        cite += " and " + name_strings[names.length-1];
-      } else {
-        cite += name_strings[0]
-      }
+      var cite =  author_string(ent, "L, I", ", ", " and ");
       cite += ", " + ent.year + ". "
-      if (fancy){
-        cite += "<b>" + ent.title + "</b>. "
-      } else {
-        cite += ent.title + ". "
-      }
-      cite += (ent.journal || ent.booktitle || "")
-      if ("volume" in ent){
-        var issue = ent.issue || ent.number;
-        issue = (issue != undefined)? "("+issue+")" : "";
-        cite += ", Vol " + ent.volume + issue;
-      }
-      if ("pages" in ent){
-        cite += ", pp. " + ent.pages
-      }
-      cite += ". "
+      cite += ent.title + ". ";
+      cite += venue_string(ent);
       if (fancy && ent.url && ent.url.slice(-4) == ".pdf") {
         cite = `${cite} <a href="${ent.url}">[pdf]</a>`
+      }
+      return cite
+    } else {
+      return "?";
+    }
+  }
+
+  function hover_cite(ent){
+    if (ent){
+      var cite = "";
+      cite += "<b>" + ent.title + "</b>";
+      if (ent.url && ent.url.slice(-4) == ".pdf") {
+        cite = `${cite} &ensp;<a href="${ent.url}" style="text-decoration:inherit"><b>[PDF]</b></a>`
+      }
+      if (ent.url && ent.url.slice(-5) == ".html") {
+        cite = `${cite} &ensp;<a href="${ent.url}" style="text-decoration:inherit"><b>[HTML]</b></a>`
+      }
+      cite += "<br>"
+      cite += author_string(ent, "I L", ", ") + ".<br>";
+      cite += venue_string(ent).trim() + " " + ent.year + ". "
+      if ("doi" in ent) {
+        cite += `<br> <a href="https://doi.org/${ent.doi}" style="text-decoration:inherit; font-size: 80%;">DOI: ${ent.doi}</a>`
       }
       return cite
     } else {
