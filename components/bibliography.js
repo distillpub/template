30b0fdd8805bca76d5f04bf0312633d0a811fa72
Christopher Olah <colah@google.com>
2017-01-10 18:03:47 -0800

    fix bibliography accent issues

--- components/bibliography.js
+++ components/bibliography.js
@@ -11,7 +11,11 @@ export default function(dom, data) {
     if(parsed) {
       parsed.forEach(e => {
         for (var k in e.entryTags){
-          e.entryTags[k] = e.entryTags[k].replace(/[\t\n ]+/g, " ");
+          var val = e.entryTags[k];
+          val = val.replace(/[\t\n ]+/g, " ");
+          val = val.replace(/{\\["^`\.'acu~Hvs]( )?([a-zA-Z])}/g,
+                            (full, x, char) => char);
+          e.entryTags[k] = val;
         }
         bibliography[e.citationKey] = e.entryTags;
         bibliography[e.citationKey].type = e.entryType;
