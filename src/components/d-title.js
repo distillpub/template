f86f1d0a91496c2c31abd66d936b08f5d731f7b5
Shan Carter <shanjcarter@gmail.com>
2017-08-28 16:16:51 -0700

    Tweaking spacing

--- src/components/d-title.js
+++ src/components/d-title.js
@@ -16,33 +16,34 @@ const T = Template('d-title', `
   padding-top: 16px;
   padding-bottom: 16px;
   margin: 0;
-  line-height: 1.3;
+  line-height: 1.15;
   font-size: 32px;
   font-weight: 700;
 }
 
 ::slotted(h2) {
   border-bottom: none !important;
+  font-size: 22px !important;
+  font-weight: 300 !important;
+  padding-top: 0 !important;
+  line-height: 1.5em !important;
+  margin-top: 0 !important;
 }
 
 @media screen and (min-width: 768px), print {
   ::slotted(h1) {
     font-size: 42px;
-    padding-bottom: 32px;
   }
 }
 @media(min-width: 1024px) {
   ::slotted(h1) {
     padding-top: 64px;
-    padding-bottom: 32px;
     font-size: 48px;
   }
 }
 @media(min-width: 1280px) {
   ::slotted(h1) {
     padding-top: 96px;
-    padding-bottom: 32px;
-    font-size: 56px;
   }
 }
 
