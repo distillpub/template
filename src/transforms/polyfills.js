70d85079387174494f1f9cfc51f5680fb5be0ed4
Ludwig Schubert <ludwigschubert@google.com>
2017-11-07 15:44:42 -0800

    Make polyfills link absolute. Release 2.2.7

--- src/transforms/polyfills.js
+++ src/transforms/polyfills.js
@@ -42,7 +42,7 @@ const addBackIn = `
 window.addEventListener('WebComponentsReady', function() {
   console.warn('WebComponentsReady');
   const loaderTag = document.createElement('script');
-  loaderTag.src = '/template.v2.js';
+  loaderTag.src = 'https://distill.pub/template.v2.js';
   document.head.insertBefore(loaderTag, document.head.firstChild);
 });
 `;
